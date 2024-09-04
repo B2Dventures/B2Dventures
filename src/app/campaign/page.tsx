24ef9d2ce3c3e909d65ce55b9c6cbf1bbc08acf5
Jirapat CHUTIMANTAPONG <jirapat.chu@ku.th>
2024-09-04 22:23:26 +0700

    create basic navbar

--- src/app/campaign/page.tsx
+++ src/app/campaign/page.tsx
@@ -1,6 +1,9 @@
+import {Header} from "@/components/Header/Header";
+
 export default function Home() {
     return (
         <main>
+            <Header/>
             <div>Work in process Campaigns page</div>
         </main>
     );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from 'react';
+import { Container, Group } from '@mantine/core';
+import classes from './Header.module.css';
+
+interface NavItemProps {
+    label: string;
+    link: string;
+}
+
+export const NavItem: React.FC<NavItemProps> = ({ label, link }) => {
+    return (
+        <a href={link} className={classes.navItem}>
+            {label}
+        </a>
+    );
+};
+
+export function Header() {
+    return (
+        <header className={classes.header}>
+            <Container size="md" className={classes.inner}>
+                <NavItem label="B2D" link="/" />
+                <Group gap={5} visibleFrom="xs">
+                    <NavItem label="Business" link="/business" />
+                    <NavItem label="Campaign" link="/campaign" />
+                </Group>
+            </Container>
+        </header>
+    );
+}
