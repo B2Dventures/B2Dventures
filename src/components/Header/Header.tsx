6df838425a06ef64aef519e8621fe60320fd3c7f
Jirapat CHUTIMANTAPONG <jirapat.chu@ku.th>
2024-09-05 16:41:33 +0700

    edit header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,32 @@
 "use client";
 
 import React from 'react';
-import { Container, Group } from '@mantine/core';
+import { Container, Group, Button } from '@mantine/core';
 import classes from './Header.module.css';
 import {NavItem} from "@/components/Header/NavItem/NavItem";
 import {Logo} from "@/components/Header/Logo/Logo";
+import { BsPerson, BsSuitcaseLg } from "react-icons/bs";
 
 export function Header() {
     return (
         <header className={classes.header}>
-            <Container className={classes.inner} >
-                <Group gap={40} visibleFrom="xs">
+            <Container className={classes.inner} fluid>
+                <Group gap={30} visibleFrom="xs" justify="center">
                     <Logo />
+                    <Button size='sm' leftSection={<BsPerson size={20} />} variant="outline" color='white'
+                            styles={{
+                                root: { color: 'black' }
+                            }} autoContrast>
                     <NavItem label="Business" link="/business" />
+                    </Button>
+                    <Button size='sm' leftSection={<BsSuitcaseLg size={20} />} variant="outline" color='white'
+                            styles={{
+                        root: { color: 'black' }
+                    }}>
                     <NavItem label="Campaign" link="/campaign" />
+                    </Button>
                 </Group>
-                <Group gap={10} visibleFrom="xs">
+                <Group gap={20} visibleFrom="xs">
                     <NavItem label="Login" link="/" />
                     <NavItem label="Signup" link="/" />
                 </Group>
