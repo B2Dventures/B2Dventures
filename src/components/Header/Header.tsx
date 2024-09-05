116fc0a6bfcfd2bd7ca86487722d4339b8be63ef
Jirapat CHUTIMANTAPONG <jirapat.chu@ku.th>
2024-09-06 00:29:28 +0700

    better style button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,28 +7,21 @@ import {NavItem} from "@/components/Header/NavItem/NavItem";
 import {Logo} from "@/components/Header/Logo/Logo";
 import { BsPerson, BsSuitcaseLg } from "react-icons/bs";
 
-
 export function Header() {
     return (
         <header className={classes.header}>
             <Container className={classes.inner} fluid>
-                <Group gap={10} visibleFrom="xs" justify="center">
+                <Group gap={30} visibleFrom="xs" justify="center">
                     <Logo />
-                    <Button size='sm' leftSection={<BsPerson size={20} />} variant="outline" color='white'
-                            styles={{
-                                root: { color: 'black' }
-                            }} autoContrast>
+                    <Button size='sm' leftSection={<BsPerson size={20} color='black'/>} variant="outline" color='white'>
                     <NavItem label="Investor" link="/investor" />
                     </Button>
-                    <Button size='sm' leftSection={<BsSuitcaseLg size={20} />} variant="outline" color='white'
-                            styles={{
-                        root: { color: 'black' }
-                    }}>
+                    <Button size='sm' leftSection={<BsSuitcaseLg size={20} color='black'/>} variant="outline" color='white'>
                     <NavItem label="Business" link="/business" />
                     </Button>
                 </Group>
                 <Group gap={20} visibleFrom="xs">
-                    <NavItem label="Login" link="/login" />
+                    <NavItem label="Login" link="/" />
                     <NavItem label="Signup" link="/" />
                 </Group>
             </Container>
