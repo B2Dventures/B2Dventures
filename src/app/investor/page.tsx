b98efae6364d66bcdcfa04d5751fbb2a35b39ffa
Jirapat CHUTIMANTAPONG <jirapat.chu@ku.th>
2024-09-11 21:09:22 +0700

    better style for invester page

--- src/app/investor/page.tsx
+++ src/app/investor/page.tsx
@@ -2,7 +2,7 @@
 
 import {Header} from "@/components/Header/Header";
 import {FundraisingCard} from "@/components/FundraisingCard/FundraisingCard";
-import { Grid, Text, Container } from '@mantine/core';
+import { Grid, Text, Container, Group, Button } from '@mantine/core';
 import classes from './investor.module.css';
 import { SearchBar } from "@/components/Search/SearchBar";
 import { baiSemiBold } from '@/app/styles/fonts'
@@ -11,15 +11,19 @@ export default function Home() {
 
     return (
         <main>
+            <Header/>
             <Container size={1440}>
-                <Header/>
-                <SearchBar/>
-                <main className={baiSemiBold.className}>
-                    <Text className={classes.font} style = {{ marginTop: '50px',marginLeft: '50px', marginBottom: '50px'}}>
-                        Live Opportunities
-                    </Text>
-                </main>
-
+                <Container fluid className={classes.searchBox}>
+                        <SearchBar/>
+                    <Group>
+                        <Button size='s' variant="outline" color='rgb(0, 0, 0, 0.6)' radius='16'>Filter</Button>
+                        <Button size='s' variant="outline" color='rgb(0, 0, 0, 0.6)' radius='16'>Sort By</Button>
+                    </Group>
+                </Container>
+                <Text className={baiSemiBold.className}
+                      style = {{ marginTop: '50px',marginLeft: '150px', marginBottom: '50px', fontSize: '35px'}}>
+                    Live Opportunities
+                </Text>
                 <Container fluid className={classes.campaign}>
                     <Grid gutter={100}>
                         <Grid.Col span={4}>
@@ -50,6 +54,36 @@ export default function Home() {
                                 raisedAmount="123,456$"
                                 investors="123"
                                 id={3}
+                            />
+                        </Grid.Col>
+                        <Grid.Col span={4}>
+                            <FundraisingCard
+                                title="B2D Ventures"
+                                description="Become a part of world's ventures"
+                                imageUrl="/1.ico"
+                                raisedAmount="123,456$"
+                                investors="123"
+                                id={4}
+                            />
+                        </Grid.Col>
+                        <Grid.Col span={4}>
+                            <FundraisingCard
+                                title="B2D Ventures"
+                                description="Become a part of world's ventures"
+                                imageUrl="/1.ico"
+                                raisedAmount="123,456$"
+                                investors="123"
+                                id={5}
+                            />
+                        </Grid.Col>
+                        <Grid.Col span={4}>
+                            <FundraisingCard
+                                title="B2D Ventures"
+                                description="Become a part of world's ventures"
+                                imageUrl="/1.ico"
+                                raisedAmount="123,456$"
+                                investors="123"
+                                id={6}
                             />
                         </Grid.Col>
                     </Grid>
