47350682f63bc0cf732e4d999a89f9acedab3bdf
Chanun Suntrapiwat <chanun.s@ku.th>
2024-09-11 22:55:56 +0700

    adding image component details page

--- src/app/investor/[id]/page.tsx
+++ src/app/investor/[id]/page.tsx
@@ -1,10 +1,42 @@
+import React from 'react';
+import {Header} from '@/components/Header/Header';
+import { InvestmentInfo } from '@/components/InvestmentInfo/InvestmentInfo';
+import {Gallery} from '@/components/Gallery/Gallery';
+import {Container, Flex, Grid} from "@mantine/core";
+
+
+
 const CampaignPage = ({ params }: { params: { id: string } }) => {
     const { id } = params;  // params = business/{id}
 
+    const images = [
+        '/1.ico',
+        '/2.ico',
+        '/3.ico'
+    ];
+
+    const highlights = [
+        'Largest platform for fractional investment in real estate.',
+        'Forbes Fintech 50 List for 2024',
+        'Investment options starting at $10'
+    ];
+
     return (
-        <div>
-            <h1>Campaign ID: {id}</h1>
-        </div>
+        <main>
+            <Header/>
+            <Container size={1250}>
+            <div>
+                <Flex>
+                    <Gallery images={images}/>
+                    <InvestmentInfo raisedAmount={772172} totalInvestors={331} daysLeft={50}/>
+                </Flex>
+
+            </div>
+            <div>
+                <h1>Campaign ID: {id}</h1>
+            </div>
+            </Container>
+        </main>
     );
 };
 
