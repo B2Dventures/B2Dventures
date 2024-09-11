e17c315076ffdac545c8b824634a575686227c3d
Chanun Suntrapiwat <chanun.s@ku.th>
2024-09-11 21:10:30 +0700

    initial login/signup page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,4 @@
-import {Container} from "@mantine/core";
+import {Button, Container, TextInput, Title} from "@mantine/core";
 import {Header} from "@/components/Header/Header";
 import React from "react";
 
@@ -6,9 +6,30 @@ export default function Home() {
     return (
         <main>
             <Header />
-            <Container size={1440} >
-                <div>Work in process Login page</div>
-            </Container>
+                <Container style={{ maxWidth: 500, marginTop: 100 }}>
+                    <Title order={2} style={{ textAlign: 'center', color: '#6C5434' }}>Login</Title>
+                    <form style={{ marginTop: 20 }}>
+                        <TextInput
+                            label="Email"
+                            placeholder="Enter your email"
+                            required
+                            size="md"
+                            style={{ marginBottom: 20 }}
+                        />
+                        <TextInput
+                            label="Password"
+                            placeholder="Enter your password"
+                            type="password"
+                            required
+                            size="md"
+                            style={{ marginBottom: 20 }}
+                        />
+                        <Button type="submit" style={{ backgroundColor: '#F7C04A', width: '100%' }}>Login</Button>
+                    </form>
+                    <div style={{ textAlign: 'center', marginTop: 20 }}>
+                        <a href="/signup" style={{ color: '#6C5434', textDecoration: 'none' }}>Don't have an account? Signup</a>
+                    </div>
+                </Container>
         </main>
     );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,14 +1,43 @@
 import {Header} from "@/components/Header/Header";
-import React from "react";
-import {Container} from "@mantine/core";
+import React from 'react';
+import { TextInput, Button, Container, Title } from '@mantine/core';
 
 export default function Home() {
     return (
         <main>
-            <Header />
-            <Container size={1440} >
-                <div>Work in process Sign up page</div>
-            </Container>
+        <Header />
+        <Container style={{ maxWidth: 500, marginTop: 100 }}>
+            <Title order={2} style={{ textAlign: 'center', color: '#6C5434' }}>Signup</Title>
+            <form style={{ marginTop: 20 }}>
+                <TextInput
+                    label="Email"
+                    placeholder="Enter your email"
+                    required
+                    size="md"
+                    style={{ marginBottom: 20 }}
+                />
+                <TextInput
+                    label="Password"
+                    placeholder="Enter your password"
+                    type="password"
+                    required
+                    size="md"
+                    style={{ marginBottom: 20 }}
+                />
+                <TextInput
+                    label="Confirm Password"
+                    placeholder="Confirm your password"
+                    type="password"
+                    required
+                    size="md"
+                    style={{ marginBottom: 20 }}
+                />
+                <Button type="submit" style={{ backgroundColor: '#F7C04A', width: '100%' }}>Signup</Button>
+            </form>
+            <div style={{ textAlign: 'center', marginTop: 20 }}>
+                <a href="/login" style={{ color: '#6C5434', textDecoration: 'none' }}>Already have an account? Login</a>
+            </div>
+        </Container>
         </main>
     );
 }
