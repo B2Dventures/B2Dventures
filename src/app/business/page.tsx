1005a8fe192db71ec1ad6d97a29962e361ace0da
Jirapat CHUTIMANTAPONG <jirapat.chu@ku.th>
2024-09-12 01:31:37 +0700

    add basic link and create campaign button

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -2,15 +2,21 @@
 
 import {Header} from "@/components/Header/Header";
 import { CampaignTable } from "@/components/CampaignTable/CampaignTable";
-import { Container } from '@mantine/core';
+import {Button, Container} from '@mantine/core';
+import classes from "./business.module.css";
+import React from "react";
+import {NavItem} from "@/components/Header/NavItem/NavItem";
 
 
 export default function Home() {
     return (
         <main>
             <Header/>
-            <Container size={1440}>
+            <Container size={1440} className={classes.container}>
                 <CampaignTable/>
+                <Button size='xs' variant="gradient" gradient={{ from: 'yellow', to: 'gold', deg: 90 }}>
+                    <NavItem label="Create Fundraising" link="/createFund" />
+                </Button>
             </Container>
         </main>
     );
