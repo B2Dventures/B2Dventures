9e2b8ae926c4fefb019509376ede3e155554f0f3
Jirapat CHUTIMANTAPONG <jirapat.chu@ku.th>
2024-09-16 01:02:57 +0700

    using devider instead of border line

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -2,7 +2,7 @@
 
 import {Header} from "@/components/Header/Header";
 import { CampaignTable } from "@/components/CampaignTable/CampaignTable";
-import {Button, Container, Grid, Group, Text} from '@mantine/core';
+import {Button, Container, Divider, Grid, Group, Text} from '@mantine/core';
 import classes from "./business.module.css";
 import React from "react";
 import {NavItem} from "@/components/Header/NavItem/NavItem";
@@ -19,29 +19,24 @@ export default function Home() {
             <footer className={classes.footer}>
                 <Container className={classes.inner} size={1440}>
                     <Group>
-                        <Grid>
-                            <Grid.Col span={4}>
-                                <div className={classes.box}>
-                                    <LuDollarSign size={50}/>
-                                    <Text className={classes.name}>Total Fund Raised</Text>
-                                    <Text className={classes.number}>$ 610,724</Text>
-                                </div>
-                            </Grid.Col>
-                            <Grid.Col span={4}>
-                                <div className={classes.box}>
-                                    <LuUsers size={50}/>
-                                    <Text className={classes.name}>Total Investors</Text>
-                                    <Text className={classes.number}>8,117</Text>
-                                </div>
-                            </Grid.Col>
-                            <Grid.Col span={4}>
-                                <div className={classes.box}>
-                                    <LuCheckCircle size={50}/>
-                                    <Text className={classes.name}>Total Campaigns</Text>
-                                    <Text className={classes.number}>8</Text>
-                                </div>
-                            </Grid.Col>
-                        </Grid>
+                        <div className={classes.box}>
+                            <LuDollarSign size={50}/>
+                            <Text className={classes.name}>Total Fund Raised</Text>
+                            <Text className={classes.number}>$ 610,724</Text>
+                        </div>
+                        <Divider orientation="vertical" />
+                        <div className={classes.box}>
+                            <LuUsers size={50}/>
+                            <Text className={classes.name}>Total Investors</Text>
+                            <Text className={classes.number}>8,117</Text>
+                        </div>
+                        <Divider orientation="vertical" />
+                        <div className={classes.box}>
+                            <LuCheckCircle size={50}/>
+                            <Text className={classes.name}>Total Campaigns</Text>
+                            <Text className={classes.number}>8</Text>
+                        </div>
+                        <Divider orientation="vertical" />
                     </Group>
                     <Group gap={20} visibleFrom="xs">
                         <Button size='md' variant="gradient" gradient={{from: 'yellow', to: 'gold', deg: 90}}>
--- src/components/InvestmentInfo/InvestmentInfo.tsx
+++ src/components/InvestmentInfo/InvestmentInfo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Text, Progress, Badge, Stack, Button} from '@mantine/core';
+import {Container, Text, Progress, Badge, Stack, Button, Divider} from '@mantine/core';
 import classes from './InvestmentInfo.module.css';
 import {LuChevronRightCircle} from "react-icons/lu";
 import {NavItem} from "@/components/Header/NavItem/NavItem";
@@ -19,16 +19,18 @@ export const InvestmentInfo: React.FC<InvestmentInfoProps> = ({ raisedAmount, go
                 <div className={classes.box}>
                     <Text className={classes.amount}>${raisedAmount.toLocaleString()}</Text>
                     <Text className={classes.goal}>of ${goalAmount.toLocaleString()}</Text>
-                    <Progress color="yellow" radius="md" size="md" value={70} animated />
                 </div>
+                <Progress color="yellow" radius="md" size="md" value={70} animated />
+                <Divider my="md" />
                 <div className={classes.box}>
                     <Text className={classes.investors}>{totalInvestors.toLocaleString()}</Text>
                     <Text className={classes.normalText}>Investors</Text>
                 </div>
+                <Divider my="md" />
                 <div className={classes.badge}>
                     <Badge color="yellow" size='24px'>{daysLeft} days left</Badge>
                 </div>
-                <div>
+                <div className={classes.badge}>
                     <Button size='lg' rightSection={<LuChevronRightCircle size={25}/>}
                             variant="outline" color='yellow' radius='20'>
                         Invest in {campaignName}
