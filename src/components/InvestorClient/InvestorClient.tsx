d826631c5ee57c57679f51bec7d7a096a2b2ffe2
Jirapat CHUTIMANTAPONG <jirapat.chu@ku.th>
2024-11-04 10:30:04 +0700

    bug fix fundraising card with anormal width

--- src/components/InvestorClient/InvestorClient.tsx
+++ src/components/InvestorClient/InvestorClient.tsx
@@ -21,22 +21,25 @@ interface InvestorClientComponentProps {
 
 const InvestorClient = ({ businesses }: InvestorClientComponentProps) => {
   return (
-    <Container fluid className={classes.campaign}>
-      <Grid gutter={100}>
-        {businesses.map(business => (
-          <Grid.Col key={business.id} span={4}>
-            <FundraisingCard
-              title={business.name}
-              description={business.description}
-              imageUrl={'/boxing_club.jpg'}
-              totalInvestment={(business.totalInvestment ? business.totalInvestment.toString() : "0")}
-              investors={business.investors.toString()}
-              id={business.id}
-            />
-          </Grid.Col>
-        ))}
-      </Grid>
-    </Container>
+      <Container fluid className={classes.campaign}>
+          <Grid gutter={100} justify="center">
+              {businesses.map((business, index) => (
+                  <Grid.Col
+                      key={business.id}
+                      span={businesses.length === 1 ? 6 : businesses.length === 2 ? 6 : 4}
+                  >
+                      <FundraisingCard
+                          title={business.name}
+                          description={business.description}
+                          imageUrl={'/boxing_club.jpg'}
+                          totalInvestment={(business.totalInvestment ? business.totalInvestment.toString() : "0")}
+                          investors={business.investors.toString()}
+                          id={business.id}
+                      />
+                  </Grid.Col>
+              ))}
+          </Grid>
+      </Container>
   );
 }
 
