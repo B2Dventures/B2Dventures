1132c4cff61f3c3c3ab88bd717ab336e10b52fd3
Chanun Suntrapiwat <chanun.s@ku.th>
2024-11-12 23:33:52 +0700

    fix:API campaing create

--- src/app/api/campaign/createFund/route.ts
+++ src/app/api/campaign/createFund/route.ts
@@ -1,9 +1,11 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/utils/db';
 import { auth } from '@clerk/nextjs/server';
 
-export async function POST(req: Request) {
-    const { userId: clerkId } = auth(req);
+export async function POST(req: NextRequest) {
+    if (auth().sessionClaims?.metadata?.role != "business") {
+        return NextResponse.json({error: "Not have authenticated"}, {status: 401});
+    }
     const body = await req.json();
 
     const {
@@ -19,55 +21,35 @@ export async function POST(req: Request) {
         opportunity
     } = body;
 
-    try {
-        const user = await prisma.user.findUnique({
-            where: { clerkId },
-            select: { id: true },
-        });
+    const id = auth().sessionClaims?.metadata?.id;
 
-        if (!user) {
-            return NextResponse.json({ error: 'User not found' }, { status: 404 });
-        }
-
-        const business = await prisma.business.findUnique({
-            where: { userId: user.id },
-            select: { id: true, approvalStatus: true },
-        });
-
-        if (!business) {
-            return NextResponse.json({ error: 'You are not a business owner' }, { status: 404 });
-        }
-
-        if (business.approvalStatus !== 'APPROVED') {
-            return NextResponse.json({ error: 'Business not approved' }, { status: 403 });
-        }
-
-        // Create the campaign and associated detail record
-        const campaign = await prisma.campaign.create({
-            data: {
-                businessId: business.id,
-                name: title,
-                description: description,
-                goal: goal,
-                min_invest: minimumInvest,
-                industry: category,
-                start_date: new Date(startDate),
-                end_date: new Date(endDate),
-                image: "https://example.com/logo-url.jpg",
-                approvalStatus: "PENDING",
-                details: {
-                    create: {
-                        highlight,
-                        product,
-                        opportunity,
-                    },
+    if (!id) {
+        return NextResponse.json({error: "User not found"}, {status: 404});
+    }
+    const campaign = await prisma.campaign.create({
+        data: {
+            businessId: id,
+            name: title,
+            description: description,
+            goal: goal,
+            min_invest: minimumInvest,
+            industry: category,
+            start_date: new Date(startDate),
+            end_date: new Date(endDate),
+            image: "https://example.com/logo-url.jpg",
+            approvalStatus: "PENDING",
+            details: {
+                create: {
+                    highlight,
+                    product,
+                    opportunity,
                 },
             },
-        });
-
-        return NextResponse.json({ success: true, campaign });
-    } catch (error) {
-        console.error('Error saving campaign:', error);
-        return NextResponse.json({ success: false, error: error.message }, { status: 500 });
+        },
+    });
+    if (!campaign) {
+        return NextResponse.json({error: "Error saving campaign"}, {status: 400});
     }
+    return NextResponse.json({ success: true, campaign });
+
 }
